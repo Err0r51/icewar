c7286c685c72495588f7df4ad591797988c8d821
Err0r51 <f.junge@outlook.com>
2024-08-15 15:02:36 +0200

    fix(env): :green_heart:  another env fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-import { env } from "node:process";
 import axios from "axios";
 import cheerio from 'cheerio';
 import type { Cheerio, CheerioAPI, Element } from "cheerio";
@@ -26,8 +25,6 @@ AxiosInstance.get("/").then((response) => {
 
         console.log(`Title: ${title}`);
         console.log(`Link: ${link}`);
-
-        console.log(env.process.DATABASE_URL);
 
         if (title && link) {
             writetodb({
